939a7fb7d9c727ff3897cb99d4794616af0fbb68
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-11-25 11:45:03 +0300

    feat: navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import { Link, NavLink } from "react-router-dom";
+import logo from "./assets/logo.png"
 import "./Navbar.css"
 
 export const Navbar = () => {
     return(
         <nav>
-            <Link to="/" className="title">Home</Link>
+            <Link to="/" className="logo">
+                <img src={logo}/>
+            </Link>
             <ul>
                 <li><NavLink to="/learning">Learning</NavLink></li>
                 <li><NavLink to="/roadmap">Roadmap</NavLink></li>
