ea6910680db55c83dc4ea1de908894ec8d91c7e9
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-11-25 12:47:23 +0300

    feat: course panel placeholder

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,37 +1,14 @@
-export const Home = () => {
-    return (
-        <div>
-            Almost heaven, West Virginia <br />
-            Blue Ridge Mountains, Shenandoah River <br />
-            Life is old there, older than the trees <br />
-            Younger than the mountains, growin' like a breeze <br />
-            Country roads, take me home <br />
-            To the place I belong <br />
-            West Virginia, mountain mama <br />
-            Take me home, country roads <br />
-            All my memories gather 'round her <br />
-            Miner's lady, stranger to blue water <br />
-            Dark and dusty, painted on the sky <br />
-            Misty taste of moonshine, teardrop in my eye <br />
-            Country roads, take me home <br />
-            To the place I belong <br />
-            West Virginia, mountain mama <br />
-            Take me home, country roads <br />
-            I hear her voice in the mornin' hour, she calls me <br />
-            The radio reminds me of my home far away <br />
-            Drivin' down the road, I get a feelin' <br />
-            That I should've been home yesterday, yesterday <br />
-            Country roads, take me home <br />
-            To the place I belong <br />
-            West Virginia, mountain mama <br />
-            Take me home, country roads <br />
-            Country roads, take me home <br />
-            To the place I belong <br />
-            West Virginia, mountain mama <br />
-            Take me home, country roads <br />
-            Take me home, (down) country roads <br />
-            Take me home, (down) country roads <br />
-        </div>
+import mf from "../assets/mf.jpg"
+import "./Home.css"
 
-    )
+export const Home = () => {
+  return (
+    <div className="courselist">
+      <div className="coursepanel">
+        <img src={mf}/>
+        <h1>Algorithms & Data Structures</h1>
+        <p>Learn the building blocks of any program</p>
+      </div>
+    </div>
+  )
 }
