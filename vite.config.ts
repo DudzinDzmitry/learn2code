9b824d4663fbd42a2d0f0aca5c56c8a03cfacdd3
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-11-30 20:45:43 +0300

    chore: remove unneeded dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Inspect from 'vite-plugin-inspect'
 import Icons from "unplugin-icons/vite"
-import IconsResolver from 'unplugin-icons/resolver'
-import AutoImport from 'unplugin-auto-import/vite'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    Inspect(),
-    AutoImport({
-      resolvers: [
-        IconsResolver({
-          extension: 'jsx',
-        }),
-      ],
-      dts: true,
-    }),
     Icons({
       compiler: 'jsx',
       jsx: 'react',
