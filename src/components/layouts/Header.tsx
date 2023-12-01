5709b08e5d45c6adeff3d76f44d99b121beecd7d
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-12-01 08:37:12 +0300

    chore: abstract tabs into list

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,13 +3,25 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Moon from '~icons/carbon/moon'
 import Sun from '~icons/carbon/sun'
 
+const data: { [name: string]: string }[] = [
+  { "label": "Home", "link": "/" },
+  { "label": "Courses", "link": "/courses" },
+  { "label": "Quizzes", "link": "/quizzes" },
+]
+
 export const Header = ({ opened, toggle }: any) => {
+  // nav content
+  const tabs = data.map((item) => (
+    <Tabs.Tab value={item.link}>{item.label}</Tabs.Tab>
+  ));
+
+  // navigation
   const navigate = useNavigate();
   const { tabVal } = useParams();
 
+  //theme change
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('light');
-
   const toggleColorScheme = () => {
     setColorScheme(computedColorScheme === "dark" ? 'light' : 'dark')
   }
@@ -19,15 +31,11 @@ export const Header = ({ opened, toggle }: any) => {
       <Group justify="space-between" style={{ padding: '7.5px 20px' }}>
         <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size="sm" />
         <Tabs
-        value={tabVal}
-        onChange={(value) => navigate(`${value}`)}
-        visibleFrom="sm"
+          value={tabVal}
+          onChange={(value) => navigate(`${value}`)}
+          visibleFrom="sm"
         >
-          <Tabs.List>
-            <Tabs.Tab value='/'>Home</Tabs.Tab>
-            <Tabs.Tab value="/learning">Learning</Tabs.Tab>
-            <Tabs.Tab value="/quiz">Quiz</Tabs.Tab>
-          </Tabs.List>
+          <Tabs.List>{tabs}</Tabs.List>
         </Tabs>
         <UnstyledButton size='sm' onClick={toggleColorScheme}>
           {computedColorScheme === "dark" ? <Sun /> : <Moon />}
