fcafa2af88fb84a01e12a733d87d2e518bc793cd
Dudzin Dzmitry <dmitriy.i.dudin@gmail.com>
2023-12-04 12:39:21 +0300

    chore: add some default courses

--- src/assets/Courses/Courses.data.tsx
+++ src/assets/Courses/Courses.data.tsx
@@ -20,4 +20,38 @@ export const COURSESDATA = [
     image: "https://i3.ytimg.com/vi/GoXwIVyNvX0/maxresdefault.jpg",
     link: "https://www.youtube.com/watch?v=GoXwIVyNvX0&list=PLWKjhJtqVAbnRT_hue-3zyiuIYj0OlpyG&ab_channel=freeCodeCamp.org",
   },
+  {
+    title: "Beginner's Tutorial for React JavaScript Library",
+    description:
+      "Learn React by building eight real-world projects and solving 140+ coding challenges.",
+    image: "https://i3.ytimg.com/vi/bMknfKXIFA8/maxresdefault.jpg",
+    link: "https://www.youtube.com/watch?v=bMknfKXIFA8&ab_channel=freeCodeCamp.org",
+  },
+  {
+    title: "React Tutorial for Beginners",
+    description:
+      "React JS Tutorial for Beginners - Learn React 18 with TypeScript and build awesome frontend app!",
+    image: "https://i3.ytimg.com/vi/SqcY0GlETPk/maxresdefault.jpg",
+    link: "https://www.youtube.com/watch?v=SqcY0GlETPk&ab_channel=ProgrammingwithMosh",
+  },
+  {
+    title: "Docker Tutorial for Beginners",
+    description:
+      "Docker Tutorial for Beginners - Learn Docker for a career in software or DevOps engineering.",
+    image: "https://i3.ytimg.com/vi/pTFZFxd4hOI/maxresdefault.jpg",
+    link: "https://www.youtube.com/watch?v=pTFZFxd4hOI&ab_channel=ProgrammingwithMosh",
+  },
+  {
+    title: "Docker Containers and Kubernetes Fundamentals",
+    description:
+      "Learn how to use Docker and Kubernetes in this complete hand-on course for beginners",
+    image: "https://i3.ytimg.com/vi/kTp5xUtcalw/maxresdefault.jpg",
+    link: "https://www.youtube.com/watch?v=kTp5xUtcalw&ab_channel=freeCodeCamp.org",
+  },
+  {
+    title: "Python Full Course for free",
+    description: "Python tutorial for beginners full course",
+    image: "https://i3.ytimg.com/vi/XKHEtdqhLK8/maxresdefault.jpg",
+    link: "https://www.youtube.com/watch?v=XKHEtdqhLK8&ab_channel=BroCode",
+  },
 ];
